0ad1d8cd29b341063f7bcfdfd55da8ed54831921
john2543home <114286922+john2543home@users.noreply.github.com>
2025-10-28 16:26:07 +0800

    fit for Fly.io direct DB

--- pdo.php
+++ pdo.php
@@ -2,9 +2,11 @@
 $dsn  = 'mysql:host=sql212.infinityfree.com;dbname=if0_39336326_shopee_log;charset=utf8mb4';
 $user = 'if0_39336326';
 $pass = 'NeHmfdukdJyk7';
+
 try {
     $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $e) {
     http_response_code(500);
-    exit('DB: ' . $e->getMessage());
+    echo json_encode(['error' => 'DB connection failed']);
+    exit;
 }
